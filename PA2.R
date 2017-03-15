f9382c10911dbde7a0256b1d6e9064e34c3631b6
Glenn Kerbein <glenn@bpwebdesign.com>
2017-03-14 19:43:49 -0700

    init project files

---
+++ PA2.R
@@ -0,0 +1,8 @@
+####	       	Replicating Data -- Peer Assessment 1	          	####
+##			                   Glenn Kerbein			                    ##
+##              			     March 4, 2017		    	                ##
+####################################################################
+
+## STEP 0: init environment
+#clean the working environment
+rm(list=ls(all=TRUE)) 
