9bf50ffbdcbe85a69f1795a0be6b0c016d3e6b90
Glenn Kerbein <glenn@bpwebdesign.com>
2017-03-14 20:03:20 -0700

    Load data

--- PA2.R
+++ PA2.R
@@ -6,3 +6,21 @@
 ## STEP 0: init environment
 #clean the working environment
 rm(list=ls(all=TRUE)) 
+
+
+
+## STEP 1: init data for reading
+# Get required files
+datafile <- "storm.csv.bz2"
+if(!file.exists(datafile)){
+  url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
+  download.file(
+    url,
+    destfile = datafile,
+    method = "curl",
+    quiet = TRUE
+  )
+}
+
+stormdata <- read.csv(datafile, header = TRUE)
+
